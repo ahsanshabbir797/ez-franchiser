9dacdcecfb73f62f84dfe411d53b3c1588c7e7b2
ahsanshabbir797 <ahsanshabbir797@gmail.com>
2024-09-05 17:46:08 +0500

    created the landing page of the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    MainPageComponent,
+   
   ],
   imports: [
     BrowserModule
